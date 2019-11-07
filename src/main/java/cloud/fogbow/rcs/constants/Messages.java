622c1f8f8bba7fd41acd1fedbfb00507462ada47
wassoares <wlfsoares@gmail.com>
2019-11-07 14:53:00 -0300

    Added new constant into Messages class

--- src/main/java/cloud/fogbow/rcs/constants/Messages.java
+++ src/main/java/cloud/fogbow/rcs/constants/Messages.java
@@ -10,5 +10,6 @@ public class Messages {
         public static final String GET_PUBLIC_KEY = "Get public key received.";
         public static final String GET_VERSION = "Get version request received.";
         public static final String GETTING_ALL_MEMBERS = "Getting all providers members.";
+        public static final String GETTING_ALL_SERVICES = "Getting all member services.";
     }
 }
